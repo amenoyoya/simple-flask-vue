b097c23b0a6efca7828f4f3202fac12e452209a0
amenoyoya <mtyswe3@gmail.com>
2019-05-20 00:18:17 +0900

    implements of authenication

--- api/frasco.py
+++ api/frasco.py
@@ -1,80 +1,164 @@
 '''
 Flask Webserver Wrapper
 '''
-import flask
-from flask import Flask, render_template, jsonify
+import flask, os
+from flask import Flask, render_template, jsonify, session, request
 from functools import wraps
+
+class Response:
+    ''' Response class '''
+    @staticmethod
+    def text(string, status=200):
+        ''' render string '''
+        return string, status
+
+    @staticmethod
+    def html(filename, status=200):
+        ''' render html file '''
+        with open(filename + '.html', 'rb') as f:
+            return f.read(), status
+    
+    @staticmethod
+    def template(filename, kwargs, status=200):
+        ''' render template html file '''
+        # template file should be in 'templates/'
+        return render_template(filename + '.html', **kwargs), status
+
+    @staticmethod
+    def json(data, status=200):
+        ''' response of json '''
+        res = jsonify(data)
+        res.status_code = status
+        return res
+    
+    @staticmethod
+    def redirect(url, status=302):
+        ''' redirect to url '''
+        return flask.redirect(url, status)
+
+
+class AuthUser:
+    ''' Basic User class for authentication
+    required static methods:
+        auth: authonicate from post data
+        save: user info to string (session_id)
+        load: string (session_id) to user info
+    '''
+    def __init__(self, username):
+        self.name = username
+
+    @staticmethod
+    def auth(data):
+        ''' static method: authonicate from post data
+        params:
+            data (dict): post data
+        return:
+            user (AuthUser): Authenicated user or None
+        '''
+        if data.get('username') == 'admin':
+            # 単純に username が 'admin' かどうかで認証
+            return AuthUser('admin')
+
+    @staticmethod
+    def save(user):
+        ''' static method: user info to string (session_id)
+        params:
+            user (AuthUser): Authenicated user
+        return:
+            session_id (str)
+        '''
+        return user.name
+    
+    @staticmethod
+    def load(session_id):
+        ''' static method: string (session_id) to user info
+        params:
+            session_id (str)
+        return:
+            user (AuthUser): Authenicated user or None
+        '''
+        return AuthUser(session_id)
+
 
 class Frasco(Flask):
     ''' Flask wrapper class '''
-    class Response:
-        ''' Response class '''
-        @classmethod
-        def text(self, string, status=200):
-            ''' render string '''
-            return string, status
+    def __init__(self, import_name, User=AuthUser, *args, **kwargs):
+        super(Frasco, self).__init__(import_name, *args, **kwargs)
+        self.config.from_object(import_name)
+        # cookieを暗号化する秘密鍵
+        self.config['SECRET_KEY'] = os.urandom(24)
+        # 認証処理用ユーザークラス
+        self.AuthUser = User
 
-        @classmethod
-        def html(self, filename, status=200):
-            ''' render html file '''
-            with open(filename + '.html', 'rb') as f:
-                return f.read(), status
-        
-        @classmethod
-        def template(self, filename, kwargs, status=200):
-            ''' render template html file '''
-            # template file should be in 'templates/'
-            return render_template(filename + '.html', **kwargs), status
-
-        @classmethod
-        def json(self, data, status=200):
-            ''' response of json '''
-            res = jsonify(data)
-            res.status_code = status
-            return res
-        
-        @classmethod
-        def redirect(self, url, status=302):
-            ''' redirect to url '''
-            return flask.redirect(url, status)
-    
-    def routes(self, route, action, methods):
-        ''' routing decorator base
-        params:
-            route: '/<int:arg>/<string:arg>/<float:arg>/<path:arg>/<uuid:arg>'
-            action: 'text' | 'html' | 'template' | 'json' | 'redirect'
-            methods: ['GET', 'POST', 'PUT', 'DELETE']
-        example:
-            @frasco.routes('/', 'template', ['GET'])
-            def index():
-                return 'template/index', {'content': 'Hello, world'}, 200
-        '''
-        def _deco(func):
-            @wraps(func) # デコレータした関数名が`wrapper`になるのを防ぐ: ルーティングを複数定義したときルートが上書きされるのを防ぐ
-            def wrapper(*args, **kwargs):
-                act = getattr(self.Response, action, None)
-                if act:
-                    act_args = func(*args, **kwargs)
-                    if isinstance(act_args, tuple):
-                        return act(*act_args)
-                    return act(act_args)
-                return '<p>Unexpected action: "' + action + '"</p>\n' \
-                    + '<p>Available actions: "text", "html", "template", "json", "redirect"</p>', 500
-            return self.route(route, methods=methods)(wrapper)
-        return _deco
-
-    def get(self, route, action):
+    def get(self, route):
         ''' get method routing decorator '''
-        return self.routes(route, action, ['GET'])
+        return self.route(route, methods=['GET'])
 
-    def post(self, route, action):
+    def post(self, route):
         ''' post method routing decorator '''
-        return self.routes(route, action, ['POST'])
+        return self.route(route, methods=['POST'])
     
-    def put(self, route, action):
+    def put(self, route):
         ''' put method routing decorator '''
-        return self.routes(route, action, ['PUT'])
+        return self.route(route, methods=['PUT'])
     
-    def delete(self, route, action):
+    def delete(self, route):
         ''' delete method routing decorator '''
-        return self.routes(route, action, ['DELETE'])
+        return self.route(route, methods=['DELETE'])
+
+    def require_login(self, err_response):
+        ''' decorator for authentication
+        params:
+            err_response (tuple): response for the case of unauthorized
+        example:
+            @frasco.post('/member')
+            @frasco.require_login(Response.redirect('/login'))
+            def member_page():
+                return Response.text('Here is a secret page')
+        '''
+        def _wrapper(func):
+            @wraps(func) # デコレートした関数名が`wrapper`になるのを防ぐ: ルーティングを複数定義したときルートが上書きされるのを防ぐ
+            def wrapper(*args, **kargs):
+                # セッション`auth`に保存されたIDからユーザー情報を取得
+                session_id = session.get('auth')
+                if session_id is None:
+                    return err_response
+                # current_user に現在ログイン中のユーザー情報を保存
+                self.current_user = self.AuthUser.load(session_id)
+                if self.current_user is None:
+                    return err_response
+                return func(*args, **kargs)
+            return wrapper
+        return _wrapper
+
+    def auth(self, login_route, logout_route, logout_response):
+        ''' decorator for login/logout process
+        params:
+            login_route (str): login route
+            logout_route (str): logout route
+            logout_response (tuple): response after logout
+        example:
+            @frasco.auth('/api/login', '/api/logout', Response.redirect('/'))
+            def login(user):
+                if user:
+                    # succeeded to login
+                    return Responce.json({'username': user.name})
+                # failed to login
+                return Response.text('Failed to login', 400)
+        '''
+        def _wrapper(func):
+            @wraps(func) # デコレートした関数名が`wrapper`になるのを防ぐ: ルーティングを複数定義したときルートが上書きされるのを防ぐ
+            def wrapper():
+                # POSTデータから認証処理実行
+                user = self.AuthUser.auth(request.form)
+                if user is not None:
+                    # セッションID保存
+                    session['auth'] = self.AuthUser.save(user)
+                return func(user)
+            return self.post(login_route)(wrapper)
+        
+        @self.get(logout_route)
+        def logout():
+            return logout_response
+        
+        return _wrapper
--- server.py
+++ server.py
@@ -1,19 +1,50 @@
-from api.frasco import Frasco
+from api.frasco import Frasco, Response
+from flask import request
 import json
 
+# 認証処理用ユーザークラス
+class User:
+    def __init__(self, username):
+        self.name = username
+
+    @staticmethod
+    def auth(data):
+        if data.get('username') == 'admin' and data.get('password') == 'pass':
+            return User('admin')
+
+    @staticmethod
+    def save(user):
+        return user.name
+    
+    @staticmethod
+    def load(session_id):
+        return User(session_id)
+
+
 # Frascoアプリケーション
-app = Frasco(__name__)
+app = Frasco(__name__, User=User)
 
-# ルートパス: index
-@app.get('/', 'html')
+# HTML
+@app.get('/')
+@app.require_login(Response.redirect('/login'))
 def index():
-    return 'html/index' # html/index.html描画
+    return Response.html('html/index') # html/index.html描画
 
-# APIルート
-@app.get('/api/<string:target>', 'json')
-def api(target):
-    with open('./api/' + target + '.json', 'rb') as f:
-        return json.load(f)
+@app.get('/login')
+def login():
+    return Response.html('html/login')
+
+# API
+@app.get('/api/users')
+def get_users():
+    with open('./api/users.json', 'rb') as f:
+        return Response.json(json.load(f))
+
+@app.auth('/api/login', '/api/logout', Response.redirect('/'))
+def auth(user):
+    if user:
+        return Response.json({'user': user.name})
+    return '', 400
 
 # Frascoサーバー実行(localhost:8000)
 if __name__ == "__main__":
